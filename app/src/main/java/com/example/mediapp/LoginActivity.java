c45e0645fa8fbdcfe22e4d8a12def079f3456dda
manojitht <manojith12345@outlook.com>
2021-03-12 11:56:08 +0530

    Settings activity update

--- app/src/main/java/com/example/mediapp/LoginActivity.java
+++ app/src/main/java/com/example/mediapp/LoginActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
